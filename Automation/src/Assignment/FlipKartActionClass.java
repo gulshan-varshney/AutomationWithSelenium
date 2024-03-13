7d37eb3f87e093c3570ef0ab2434d17adfaf3c83
gulsh <gulsh@Gulshan.Dlink>
2024-03-14 02:32:15 +0530

    first commit

---
+++ Automation/src/Assignment/FlipKartActionClass.java
@@ -0,0 +1,38 @@
+package Assignment;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class FlipKartActionClass {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.flipkart.com/");
+		
+		WebElement fashion = driver.findElement(By.xpath("//span[text()='Fashion']"));
+		
+		Actions act = new Actions(driver);
+		
+		act.moveToElement(fashion).perform();
+		
+		driver.findElement(By.xpath("//a[text()='Men Footwear']")).click();
+		
+		WebElement productName = driver.findElement(By.xpath("(//a[@class='IRpwTa'])[1]"));
+		WebElement productPrice = driver.findElement(By.xpath("(//div[@class='_30jeq3'])[1]"));
+		System.out.println(productName.getText()+"----->"+productPrice.getText());
+		
+		driver.findElement(By.xpath("//a[text()='Careers']")).click();
+		driver.findElement(By.id("candidate-login-before")).click();
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("gulshan@gmail.com");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("gulshan@123");
+		driver.findElement(By.xpath("//iframe[@title='reCAPTCHA']")).click();
+		driver.findElement(By.xpath("//button[@class='btn_submit f-w-600']")).click();
+	}
+}
