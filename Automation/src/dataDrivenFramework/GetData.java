2a6d97e21dc973517926f430413d835eadfd464a
gulsh <gulsh@Gulshan.Dlink>
2024-04-12 22:25:56 +0530

    framework

---
+++ Automation/src/dataDrivenFramework/GetData.java
@@ -0,0 +1,17 @@
+package dataDrivenFramework;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class GetData {
+
+	public String getdatafrompropertyfile(String key) throws IOException {
+		
+		FileInputStream fis= new FileInputStream("./testData/CommonData.properties");
+		Properties prop = new Properties();
+		prop.load(fis);
+		String value = prop.getProperty(key);
+		return value;
+		}
+}
