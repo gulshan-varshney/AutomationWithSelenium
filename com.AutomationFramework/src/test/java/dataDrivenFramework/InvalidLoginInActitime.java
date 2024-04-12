13b3b85a356b095456b3bf1a30f66ea6920e4ecd
gulsh <gulsh@Gulshan.Dlink>
2024-04-13 03:29:00 +0530

    invalidlogin

--- com.AutomationFramework/src/test/java/dataDrivenFramework/InvalidLoginInActitime.java
+++ com.AutomationFramework/src/test/java/dataDrivenFramework/InvalidLoginInActitime.java
@@ -1,6 +1,34 @@
 package dataDrivenFramework;
 
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class InvalidLoginInActitime {
 
-	
+	public static void main(String[] args) throws IOException {
+		
+		Flib lib = new Flib();
+		String url = lib.getDataFromPropertyFile("./src/test/resources/CommonData.properties", "url");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get(url);
+		
+		int rowCount = lib.getRowCount("./src/test/resources/ActiTimeData.xlsx", "InvalidData");
+		
+		for(int i=0;i<=rowCount;i++) {
+			
+			String username = lib.getDataFromExcelFile("./src/test/resources/ActiTimeData.xlsx", "InvalidData", i, 0);
+			String password = lib.getDataFromExcelFile("./src/test/resources/ActiTimeData.xlsx", "InvalidData", i, 1);
+			
+			driver.findElement(By.name("username")).sendKeys(username);
+			driver.findElement(By.name("pwd")).sendKeys(password);
+			driver.findElement(By.id("loginButton")).click();
+		}
+	}
 }
